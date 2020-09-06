965c7a03fc3ddf8c89064889586c74bff65ba076
Eddo091 <44987358+eddo091@users.noreply.github.com>
2020-09-05 20:07:31 -0600

    Tabhost 2 + Datos en Valores.java

--- app/src/main/java/com/example/prueba/MainActivity.java
+++ app/src/main/java/com/example/prueba/MainActivity.java
@@ -23,34 +23,50 @@ import java.util.Objects;
 public class MainActivity extends Activity {
     public
     TabHost TbhConversorUno_;
-    Valores misvalores= new Valores();
+    TabHost ConversorSuperficie;
+    Valores misvalores = new Valores();
+    Valores.Superficie Superficie= new Valores.Superficie();
     EditText num1_;
     TextView Resp_;
+
+    //"Super" significa Superficie.
+    EditText num1Super;
+    TextView RespSuper;
 
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        num1_=findViewById( R.id.txtnum1_ );
-        Resp_=findViewById( R.id.lblrespuesta_ );
-        TbhConversorUno_=findViewById( R.id.TbhConversor );
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        num1_ = findViewById( R.id.txtnum1_ );
+        Resp_ = findViewById( R.id.lblrespuesta_ );
+        TbhConversorUno_ = findViewById( R.id.TbhConversor );
         TbhConversorUno_.setup();
-        TbhConversorUno_.addTab(TbhConversorUno_.newTabSpec("Monedas").setContent(R.id.TbhConversor).setIndicator("Monedas", null));
-        TbhConversorUno_.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+        TbhConversorUno_.addTab( TbhConversorUno_.newTabSpec( "Monedas" ).setContent( R.id.TbhConversor ).setIndicator( "Monedas", null ) );
+        TbhConversorUno_.setOnTabChangedListener( new TabHost.OnTabChangeListener() {
             @Override
             public void onTabChanged(String s) {
                 num1_.getText().clear();
-                Resp_.setText("");
+                Resp_.setText( "" );
             }
-        });
+        } );
+        ConversorSuperficie=findViewById( R.id.ConversorSuperficie );
+        ConversorSuperficie.setup();
+        ConversorSuperficie.addTab( ConversorSuperficie.newTabSpec( "Superficie" ).setContent( R.id.ConversorSuperficie ).setIndicator( "Superficie", null ) );
+        ConversorSuperficie.setOnTabChangedListener( new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String g) {
+                num1Super.getText().clear();
+                RespSuper.setText("");
+            }
+        } );
 
     }
+
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
 
 
-
-    public void NuevaConversion(View view){
+    public void NuevaConversion(View view) {
         LimpiarTexto();
 
 
@@ -59,52 +75,87 @@ public class MainActivity extends Activity {
     private void LimpiarTexto() {
         num1_.setText( "" );
         Resp_.setText( "" );
+        num1Super.setText( "" );
+        RespSuper.setText( "" );
     }
 
     ;
 
-    private void validacion() {
-        String num1__= num1_.getText().toString();
-        if (num1__.equals( "" )){
+    private void validacion_() {
+        String num1__ = num1_.getText().toString();
+        String SuperNum= num1Super.getText().toString();
+        if (num1__.equals( "" )) {
             num1_.setError( "Ingrese cantidad" );
+            if (SuperNum.equals( "" )){
+                num1Super.setError( "Ingrese Valor" );
+
+            }
+
         }
         ;
+
+
     }
+
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public void Convertir(View view) {
         try {
 
-            TextView tmpVal= (TextView) findViewById( R.id.txtnum1_ );
+            TextView tmpVal = (TextView) findViewById( R.id.txtnum1_ );
             double cantidad = Double.parseDouble( tmpVal.getText().toString() );
-            validacion();
+            validacion_();
             int de = 0, a = 0;
             double resp = 0;
 
-            switch (Objects.requireNonNull( TbhConversorUno_.getCurrentTabTag() )){
+            switch (Objects.requireNonNull( TbhConversorUno_.getCurrentTabTag() )) {
                 case "Monedas":
-                    misvalores.val = (Spinner) findViewById(R.id.cboUno2);
+                    misvalores.val = (Spinner) findViewById( R.id.cboUno2 );
                     de = misvalores.val.getSelectedItemPosition();
-                    misvalores.val = (Spinner) findViewById(R.id.cboDos2);
-                    a= misvalores.val.getSelectedItemPosition();
-                    resp= misvalores.datos[0][a]/misvalores.datos[0][de];
+                    misvalores.val = (Spinner) findViewById( R.id.cboDos2 );
+                    a = misvalores.val.getSelectedItemPosition();
+                    resp = misvalores.datos[0][a] / misvalores.datos[0][de];
 
                     break;
 
             }
 
-            tmpVal = (TextView) findViewById(R.id.lblrespuesta_);
-            tmpVal.setText(String.format("Respuesta: " + resp));
+            tmpVal = (TextView) findViewById( R.id.lblrespuesta_ );
+            tmpVal.setText( String.format( "Respuesta: " + resp ) );
 
         } catch (Exception e) {
-
-            TextView temp = (TextView) findViewById(R.id.lblrespuesta_);
-
-
-            Toast.makeText(getApplicationContext(),"Error: Ingrese la cantidad",Toast.LENGTH_LONG).show();
+            TextView temp = (TextView) findViewById( R.id.lblrespuesta_ );
+            Toast.makeText( getApplicationContext(), "Error: Ingrese la cantidad", Toast.LENGTH_LONG ).show();
         }
-
     }
 
-    ;
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void SuperConvertir(View view) {
+        try{
+        TextView TmpVal2 =(TextView) findViewById( R.id.txtnumSuper ) ;
+        double cant= Double.parseDouble( TmpVal2.getText().toString() );
+        validacion_();
+        int de = 0, a = 0;
+        double resp = 0;
+        switch (ConversorSuperficie.getCurrentTabTag()) {
+            case "Superficie":
+                Superficie.val2=(Spinner)findViewById( R.id.cboUno );
+                de=Superficie.val2.getSelectedItemPosition();
+                Superficie.val2=(Spinner)findViewById( R.id.cboDos );
+                a= Superficie.val2.getSelectedItemPosition();
+                resp=Superficie.SuperDatos[0][a]/Superficie.SuperDatos[0][de];
+                break;
+
+        }
+        TmpVal2= (TextView)findViewById( R.id.txtResultado );
+        TmpVal2.setText( String.format( "Respuesta: " +resp ) );
+
+        }catch (Exception err){
+            TextView temp =(TextView) findViewById( R.id.txtResultado );
+            Toast.makeText( getApplicationContext(),"Error: Ingrese dato", Toast.LENGTH_LONG ).show();
+        }
+
+
+
+    }
 }
--- app/src/main/java/com/example/prueba/Valores.java
+++ app/src/main/java/com/example/prueba/Valores.java
@@ -3,8 +3,7 @@ package com.example.prueba;
 import android.widget.Spinner;
 
 public class Valores {
-        Spinner spUno_;
-        Spinner spDos_;
+        public
 
         Spinner val;
 
@@ -17,4 +16,24 @@ public class Valores {
 
         };
 
+
+        public static class  Superficie {
+
+                Spinner  val2;
+
+
+                double[][] SuperDatos= new double[][]{
+                        new double[]{1,0.11,0.111111,0.092903,0.00014774656489,0.000013292827,0.000009290304},//Pie Cuadrado
+                        new double[]{7.5625302466,1,0.84028113851,0.70258205,0.0011173378658,0.00010052741037, 0.000070258205},//Vara Cuadrada
+                        new double[]{9,1.1900778849,1,0.83612736,0.001329719084,0.000119635447,0.000083612736},//Yarda Cuadrada
+                        new double[]{10.763910417,1.4233213046,1.1959900463,1,0.0015903307888,0.00014308280488,0.0001},//Metro Cuadrado
+                        new double[]{6768.34687,894.98443634,752.03854111,628.8,1,0.089970467709,0.06288},//Tareas
+                        new double[]{75228.539346,9947.5356651,8358.726594,6988.96,11.11475827,1,0.698896},//Manzana
+                        new double[]{107639.10417,14233.213046,11959.900463,10000,15.903307888,1.4308280488,1},//Hectareas
+
+                };
+
+        };
+
 }
+
